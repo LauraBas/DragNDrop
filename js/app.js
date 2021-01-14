face4cd49c8bfa6522d2aeae727f940e7dc9b257
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-14 15:00:18 +0000

    Added eventListeners and implementation

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ fill.addEventListener('dragstart',dragStart)
 fill.addEventListener('dragend',dragEnd)
 
 for(const empty of empties) {
-    
-
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
 }
 
 function dragStart() {
@@ -15,21 +17,23 @@ function dragStart() {
 }
 
 function dragEnd() {
-    this.className = 'fill'
+    this.className = 'fill';
 }
 
 function dragOver(e) {
-
+    e.preventDefault();
 }
 
 function dragEnter(e) {
-
+    e.preventDefault();
+    this.className += ' hovered';    
 }
 
 function dragLeave() {
-
+    this.className = 'empty';
 }
 
 function dragDrop() {
-
+  this.className = 'empty';
+  this.append(fill);
 }
